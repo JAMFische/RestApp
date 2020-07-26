1cf602db5f4b093060be7a2071407c9b2b2850b3
AlexRyzhkova <alexandra.ryzhkova@gmail.com>
2020-07-26 16:32:46 +0200

    Add buttons for hall selection

---
+++ src/components/hallSelector/hallSelector.stories.js
@@ -0,0 +1,6 @@
+import "./hallSelector.css";
+import hallSelector from "./hallSelector.html";
+
+export default { title: "Components/hallSelector" };
+
+export const Reservation_hall = () => hallSelector;
